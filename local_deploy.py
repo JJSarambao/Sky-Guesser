aec15a5f362cd7083110573a515fd40966bd5811
Jesriel John Sarambao <msarambao@gmail.com>
2023-10-12 07:04:28 +0800

    Update local_deploy.py

--- local_deploy.py
+++ local_deploy.py
@@ -1,8 +1,9 @@
 import random as rand
-import streamlit as st
-import tensorflow as tf
+
 import cv2
 import numpy as np
+import streamlit as st
+import tensorflow as tf
 from PIL import Image, ImageOps
 
 
