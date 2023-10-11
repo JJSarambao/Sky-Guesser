83cf402325b9d8c46f76dfb71fece15f44c37022
Jesriel John Sarambao <msarambao@gmail.com>
2023-10-12 01:15:02 +0800

    Update local_deploy.py

--- local_deploy.py
+++ local_deploy.py
@@ -14,7 +14,7 @@ model = load_model()
 classes = {0: "Cloudy", 1: "Rainy", 2: "Shiny", 3: "Sunrise"}
 
 st.write(
-  """
+    """
 # Sky Guesser - Sky Classifier
 ### Jesriel John Sarambao - CPE32S6
 ### Emerging Technologies 2 in CpE
@@ -24,29 +24,29 @@ st.write(
 def say_something(class_val):
     what_to_say = {
         0: [
-            "Good time for a walk!", 
-            "It might rain though. Take care!", 
-            "You can do anything you want right now.", 
+            "Good time for a walk!",
+            "It might rain though. Take care!",
+            "You can do anything you want right now.",
             "Have a nice day!"
         ],   
         1: [
-            "N.. not a good time for a walk", 
-            "You.. you can go out and play in the rain if you want", 
-            "Make sure you have your umbrellas today!", 
-            "Take care! Roads and sidewalks may get slippery at times.", 
+            "N.. not a good time for a walk",
+            "You.. you can go out and play in the rain if you want",
+            "Make sure you have your umbrellas today!",
+            "Take care! Roads and sidewalks may get slippery at times.",
             "A great day for a hot soup, isn't it?"
         ],   
         2: [
-            "Shiiiiiiiiny~!", "What a nice time to walk outside.", 
-            "It might get too hot, make sure you're hydrated!", 
-            "Take an umbrella with you, its for the better", 
+            "Shiiiiiiiiny~!", "What a nice time to walk outside.",
+            "It might get too hot, make sure you're hydrated!",
+            "Take an umbrella with you, its for the better",
             "AH-- I'M BURNING-- wait, I'm not a mob from Minecraft.",
             "A great time for a nice cold glass of juice. Or water. Or tea. Anything. Just drink."
         ],
         3: [
-            "Wake up, sleepy head.", 
-            "A nice new start for the day! Do your best!", 
-            "YOU'RE GONNA BE LATE! YOU'RE GONNA BE-- nah, I'm just kidding, prepare at your own pace.", 
+            "Wake up, sleepy head.",
+            "A nice new start for the day! Do your best!",
+            "YOU'RE GONNA BE LATE! YOU'RE GONNA BE-- nah, I'm just kidding, prepare at your own pace.",
             "Rise and Shine! No, don't do that literally--"
         ]
     }
@@ -82,5 +82,5 @@ else:
     string = f"Hmm.. looks like {class_names[np.argmax(prediction)]} sky to me! {say_something(np.argmax(prediction))}"
     st.success(string)
     st.info(
-      "I should have uploaded a YOLOv5 model actually. Thing is.. I have problems making it work outside the YOLOv5 folder :("
+        "I should have uploaded a YOLOv5 model actually. Thing is.. I have problems making it work outside the YOLOv5 folder :("
     )
